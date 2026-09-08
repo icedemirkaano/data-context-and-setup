e0cb04a4fd7d9895151721c5e7f74cc926863768
Emir Kaan Üngül <kaanungl@gmail.com>
2026-09-08 10:51:42 +0300

    Implement get_wait_time method

--- olist/order.py
+++ olist/order.py
@@ -20,7 +20,30 @@ class Order:
         and filters out non-delivered orders unless specified
         """
         # Hint: Within this instance method, you have access to the instance of the class Order in the variable self, as well as all its attributes
-        pass  # YOUR CODE HERE
+        orders = self.data['orders'].copy()
+
+        if is_delivered:
+            orders = orders.query("order_status == 'delivered'").copy()
+
+        orders['order_purchase_timestamp'] = pd.to_datetime(orders['order_purchase_timestamp'])
+        orders['order_delivered_customer_date'] = pd.to_datetime(orders['order_delivered_customer_date'])
+        orders['order_estimated_delivery_date'] = pd.to_datetime(orders['order_estimated_delivery_date'])
+
+        orders['wait_time'] = (
+            orders['order_delivered_customer_date'] - orders['order_purchase_timestamp']
+        ) / np.timedelta64(1, 'D')
+
+        orders['expected_wait_time'] = (
+            orders['order_estimated_delivery_date'] - orders['order_purchase_timestamp']
+        ) / np.timedelta64(1, 'D')
+
+        orders['delay_vs_expected'] = (
+            (orders['order_delivered_customer_date'] - orders['order_estimated_delivery_date'])
+            / np.timedelta64(1, 'D')
+        ).apply(lambda x: x if x > 0 else 0)
+
+        return orders[['order_id', 'wait_time', 'expected_wait_time',
+                       'delay_vs_expected', 'order_status']]
 
     def get_review_score(self):
         """
